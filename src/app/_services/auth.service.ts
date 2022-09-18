2d067b2be188702d6b132dd0dd322e93f3855f9a
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-18 21:16:16 +0200

    Security: Start securizing the site

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'https://easy-templates-backend.herokuapp.com/'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  user:any = null;
+
+  login(email: string, password: string): Observable<any> {
+    
+    this.user = null;
+
+    this.user = {
+      "email": email,
+      "password": password
+    };
+
+    return this.http.post(AUTH_API + 'login', JSON.stringify(this.user), { headers: { 'Content-Type': 'application/json'}});
+  }
+
+  /*findRole(username: string): Observable<any> {
+    return this.http.get(AUTH_API + "users/" + username);
+  }*/
+
+
+  /*register(username: string, email: string, password: string): Observable<any> {
+    return this.http.post(AUTH_API + 'register', {
+      username,
+      email,
+      password
+    }, httpOptions);
+  }*/
+}
