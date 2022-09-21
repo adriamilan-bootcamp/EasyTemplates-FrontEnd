7575af1b740d8655e9905af8531ec3668f553f5a
vladasel <ionelavladasel2710@gmail.com>
2022-09-21 23:49:27 +0200

    user-dashboard in process->img-user

--- src/app/img-user/img-user.component.ts
+++ src/app/img-user/img-user.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { LogOutComponent } from '../log-out/log-out.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Image } from '../models/image.model';
+import { ImageService } from '../_services/image.service';
 
 @Component({
   selector: 'app-img-user',
@@ -7,9 +13,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgUserComponent implements OnInit {
 
-  constructor() { }
+  imgs?: Image[];
+  img?: Image = {
+    id: '',
+    date: '',
+    src: ''
+
+
+  };
+
+
+  valuePlaceholder: any;
+  public formSearch: FormGroup;
+  public default = 'select';
+
+  constructor(private imgService: ImageService, private fb: FormBuilder, private router: Router) {
+    this.formSearch = this.fb.group({
+      options: '',
+
+    })
+  }
 
   ngOnInit(): void {
+    this.seeByUser(71); 
+  }
+
+
+
+  onChange(f: any) {
+    console.log(f.value);
+    this.valuePlaceholder = f.value['options'];
+  }
+
+
+
+  seeByUser(id:any) {
+ 
+    this.imgService.getByUserId(id)
+      .subscribe(
+        data => {
+          this.imgs = data;
+          console.log("templates: " + data);
+        },
+        error => {
+          console.log("error listar templates: " + error);
+
+        }
+      );
+    (<HTMLInputElement>document.getElementById("inputI")).value = '';
+  }
+
+
+  delete(id: any) {
+
+
+    let c = "Are you sure you want to delete?"
+    if (confirm(c) == true) {
+      this.imgService.delete(id)
+        .subscribe(
+          response => {
+            console.log("respuesta eliminar user->" + response);
+            window.location.reload();
+            alert("User deleted");
+
+          }, error => {
+            console.log(error);
+
+          }
+        );
+    } else {
+      window.location.reload();
+    }
+
   }
 
 }
