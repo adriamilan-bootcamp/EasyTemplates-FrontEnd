17702ae7b860a3c870648fd5b594c71abc21ae89
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-22 12:39:17 +0200

    Parent -> Child on Home

--- src/app/nav-user/nav-user.component.ts
+++ src/app/nav-user/nav-user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+
+import { HomeUserComponent } from '../home-user/home-user.component';
 
 @Component({
   selector: 'app-nav-user',
@@ -8,6 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class NavUserComponent implements OnInit {
 
   constructor() { }
+
+  item = "home";
+
+  addItem(newItem: string) {
+    //this.item = newItem;
+    console.log(newItem);
+    switch (newItem) {
+      case 'user':
+        ($('#myTab a[href="#profile-tab"]')as any).tab('show')
+        break;
+
+      case 'templates':
+        ($('#myTab a[href="#templates-tab"]')as any).tab('show')
+        break;
+
+      case 'pdfs':
+        ($('#myTab a[href="#pdfs-tab"]')as any).tab('show')
+        break;
+
+      case 'imgs-tab':
+        ($('#myTab a[href="#profile"]')as any).tab('show')
+        break;
+
+      default:
+        break;
+    }
+  }
 
   ngOnInit(): void {
   }
