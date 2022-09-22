169c5cad31b82782d317e84007044cfa3b6f7304
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-09-22 12:48:37 +0200

    Add JQuery

--- src/app/nav-user/nav-user.component.ts
+++ src/app/nav-user/nav-user.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 import { HomeUserComponent } from '../home-user/home-user.component';
 
+import * as $ from 'jquery';
+
+
 @Component({
   selector: 'app-nav-user',
   templateUrl: './nav-user.component.html',
@@ -18,19 +21,19 @@ export class NavUserComponent implements OnInit {
     console.log(newItem);
     switch (newItem) {
       case 'user':
-        ($('#myTab a[href="#profile-tab"]')as any).tab('show')
+        //($('#myTab a[href="#profile-tab"]')as any).tab('show')
         break;
 
       case 'templates':
-        ($('#myTab a[href="#templates-tab"]')as any).tab('show')
+        //($('#myTab a[href="#templates-tab"]')as any).tab('show')
         break;
 
       case 'pdfs':
-        ($('#myTab a[href="#pdfs-tab"]')as any).tab('show')
+        //($('#myTab a[href="#pdfs-tab"]')as any).tab('show')
         break;
 
       case 'imgs-tab':
-        ($('#myTab a[href="#profile"]')as any).tab('show')
+        //($('#myTab a[href="#profile"]')as any).tab('show')
         break;
 
       default:
