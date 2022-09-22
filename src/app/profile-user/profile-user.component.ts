13a065741ea53d09be8d7e793622ad34288f4762
vladasel <ionelavladasel2710@gmail.com>
2022-09-22 14:49:09 +0200

    show info user in user-dashboard

--- src/app/profile-user/profile-user.component.ts
+++ src/app/profile-user/profile-user.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LogOutComponent } from '../log-out/log-out.component';
+import { Users } from '../models/users.model';
+import { UsersService } from '../_services/users.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SecurityService } from '../_services/security.service';
 
 @Component({
   selector: 'app-profile-user',
@@ -6,10 +13,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-user.component.css']
 })
 export class ProfileUserComponent implements OnInit {
+  user?: Users = {
+    id: '',
+    email: '',
+    firma: '',
+    password: '',
+    roles: '',
+    username: ''
 
-  constructor() { }
+
+  };
+  valuePlaceholder: any;
+  public formSearch: FormGroup;
+  public default = 'select';
+  constructor(private usersService: UsersService, private fb: FormBuilder, private router: Router,private security:SecurityService) { 
+    this.formSearch = this.fb.group({
+      options: '',
+
+    })
+  }
+
 
   ngOnInit(): void {
+    this.userById();
+  }
+
+  userById() { 
+    this.usersService.getUserByID(this.security.getId())
+      .subscribe(
+        data => {
+          this.user = data;
+          console.log("usuarios: " + JSON.stringify(data));
+
+        },
+        error => {
+          console.log("error listar usuarios: " + error);
+
+        }
+      );
+  
   }
 
 }
