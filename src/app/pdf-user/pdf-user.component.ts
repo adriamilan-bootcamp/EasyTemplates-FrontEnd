0a399cd5883fef2278325e68d81380bfbc0cd27c
vladasel <ionelavladasel2710@gmail.com>
2022-09-23 20:20:17 +0200

    see and delete pdf in view user

--- src/app/pdf-user/pdf-user.component.ts
+++ src/app/pdf-user/pdf-user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Pdf } from '../models/pdf.model';
+import { PdfService } from '../_services/pdf.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SecurityService } from '../_services/security.service';
 
 @Component({
   selector: 'app-pdf-user',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdf-user.component.css']
 })
 export class PdfUserComponent implements OnInit {
+  pdfs?: Pdf[];
 
-  constructor() { }
+  constructor(private pdfService: PdfService, private fb: FormBuilder, private router: Router, private secService: SecurityService) { }
 
   ngOnInit(): void {
+    this.seePdfs(this.secService.getId())
+  }
+
+  seePdfs(id: any) {
+    this.pdfService.getByUserId(id)
+      .subscribe(
+        data => {
+          this.pdfs = data;
+          console.log("Datos pdf: " + JSON.stringify(data))
+
+        },
+        error => {
+          console.log("error listar pdfs: " + JSON.stringify(error));
+
+        }
+      );
+  }
+
+  deletePdf(id: any) {
+    let c = "Are you sure you want to delete?"
+    if (confirm(c) == true) {
+      this.pdfService.delete(id)
+        .subscribe(
+          response => {
+            console.log("respuesta eliminar pdf->" + response);
+            this.ngOnInit();
+            alert("Pdf deleted!");
+
+
+          }, error => {
+            alert("Something went wrong could not delete the pdf.")
+            console.log(error);
+
+          }
+        );
+    } else {
+      this.ngOnInit();
+    }
+
   }
 
 }
