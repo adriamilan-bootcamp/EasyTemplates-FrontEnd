02ec1045f9a75199e48e587c176c2db5d2bc2fef
vladasel <ionelavladasel2710@gmail.com>
2022-09-23 21:06:04 +0200

    see and delete template from view user

--- src/app/template-user/template-user.component.ts
+++ src/app/template-user/template-user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Template } from '../models/template.model';
+import { TemplatesService } from '../_services/templates.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SecurityService } from '../_services/security.service';
 
 @Component({
   selector: 'app-template-user',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-user.component.css']
 })
 export class TemplateUserComponent implements OnInit {
-
-  constructor() { }
+  templ?: Template[];
+  constructor(private templService: TemplatesService, private fb: FormBuilder, private router: Router, private secService: SecurityService) { }
 
   ngOnInit(): void {
+
+    this.seeTemplates(this.secService.getId())
+  }
+
+  seeTemplates(id: any) {
+    this.templService.getTemplateByUserId(id)
+      .subscribe(
+        data => {
+          this.templ = data;
+          console.log("Datos templates: " + JSON.stringify(data))
+
+        },
+        error => {
+          console.log("error listar templates: " + JSON.stringify(error));
+
+        }
+      );
+  }
+
+  deleteTemplate(id: any) {
+    let c = "Are you sure you want to delete?"
+    if (confirm(c) == true) {
+      this.templService.deleteTemplate(id)
+        .subscribe(
+          response => {
+            console.log("respuesta eliminar template->" + response);
+            this.ngOnInit();
+            alert("Template deleted!");
+
+
+          }, error => {
+            alert("Something went wrong could not delete the template.")
+            console.log(error);
+
+          }
+        );
+    } else {
+      this.ngOnInit();
+    }
+
   }
 
 }
